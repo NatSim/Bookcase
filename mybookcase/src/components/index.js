785192b093cd76b634802e538bc00d5dfc983e2b
NatSim <natsimbig@gmail.com>
2020-10-14 21:27:56 +0100

    File path sorting

--- mybookcase/src/components/index.js
+++ mybookcase/src/components/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from './components/index';
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/index';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +9,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+
+export default App;
