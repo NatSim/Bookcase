10950acb44faee2a3f92c70ccb9ecc389b27a684
NatSim <natsimbig@gmail.com>
2020-12-17 22:56:34 +0000

    adding image slider works fine,adding header fixed needs work

--- mybookcase/src/components/Banner.js
+++ mybookcase/src/components/Banner.js
@@ -1,15 +1,30 @@
 import React from "react";
-import Image from "../Components/images/Banner.jpeg";
+import Image1 from "../Components/images/Banner.jpeg";
+import Image2 from "../Components/images/Banner2.jpeg";
+import "./Banner.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+// import "react-slideshow-image/dist/styles.css";
 
-const Banner = () => {
+const slideImages = [{ Image1 }, { Image2 }];
+
+const Slideshow = () => {
   return (
-    <>
-      <div className="row-home"></div>
-      <div>
-        <img className="banner-xmas" src={Image} alt="Christmas Book set" />
-      </div>
-    </>
+    <div className="row-home-container">
+      <Slide>
+        <div className="carousel-slide ">
+          <div style={{ backgroundImage: `url(${Image1[0]})` }}>
+            <img src={Image1}></img>
+          </div>
+        </div>
+        <div className="carousel-slide ">
+          <div style={{ backgroundImage: `url(${Image2[1]})` }}>
+            <img src={Image2}></img>
+          </div>
+        </div>
+      </Slide>
+    </div>
   );
 };
 
-export default Banner;
+export default Slideshow;
