4d1517a161e8550b5d190a738f5c6f86c78c3269
NatSim <natsimbig@gmail.com>
2021-02-16 15:19:29 +0000

    adding search js file

---
+++ mybookcase/src/components/search.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../pages/app.css";
+
+const Search = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.findBooks(props.keyword);
+  };
+
+  // const FixButton = () => {
+  //   if (Search === true) {
+  //     "search-button".classList.add("fix-button");
+  //   }
+  // };
+
+  return (
+    <div>
+      <div className="store-title">
+        <h1>Online Bookcase</h1>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <h1>{props.keyword && "Searching for keyword: " + props.keyword}</h1>
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Title,author,keyword or ISBN"
+            value={props.keyword}
+            onChange={(e) => props.setKeyword(e.target.value)}
+          />
+          <input className="search-button" type="submit" value="Find" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
