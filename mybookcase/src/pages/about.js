e651e6192dabebda2be87dc15e430865f6b4f658
NatSim <natsimbig@gmail.com>
2021-05-21 13:32:00 +0100

    test new config push

--- mybookcase/src/pages/about.js
+++ mybookcase/src/pages/about.js
@@ -8,8 +8,9 @@ const About = () => {
       <p>
         The following application was created by me, Natasha Simone. This
         bookcase app displays a list of books that a user can save to a local
-        bookcase. Click on the "Add+" button to add a book to your bookcase. Use
-        the search bar to find the latest books by name, author or description.
+        bookcase. Click on the "Add +" button to add a book to your bookcase.
+        Use the search bar to find the latest books by name, author or
+        description.
       </p>
     </div>
   );
